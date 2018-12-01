830b2f3f92380952d769a626af3a5d690457e5e5
Rob <tiker@funkymonkey.org>
2018-11-30 22:16:02 -0500

    Indexed destinations

--- UnderbellyPortals.lua
+++ UnderbellyPortals.lua
@@ -5,12 +5,12 @@ local HBDP = LibStub('HereBeDragons-Pins-2.0')
 
 local UNDERBELLY = 628
 local destinations = {
-	{x = 0.69, y = 0.57, name = L['Sewer Guard Station']},
-	{x = 0.69, y = 0.17, name = L['Black Market']},
-	{x = 0.67, y = 0.70, name = L['Inn Entrance']},
-	{x = 0.78, y = 0.80, name = L['Alchemists\' Lair']},
-	{x = 0.43, y = 0.52, name = L['Abandoned Shack']},
-	{x = 0.306, y = 0.467, name = L['Rear Entrance']}
+	[L['Sewer Guard Station']]	= {x = 0.69, y = 0.57},
+	[L['Black Market']]			= {x = 0.69, y = 0.17},
+	[L['Inn Entrance']]			= {x = 0.67, y = 0.70},
+	[L['Alchemists\' Lair']]	= {x = 0.78, y = 0.80},
+	[L['Abandoned Shack']]		= {x = 0.43, y = 0.52},
+	[L['Rear Entrance']]		= {x = 0.306, y = 0.467}
 }
 
 local PlayerMarker = CreateFrame('Frame')
@@ -36,20 +36,17 @@ hooksecurefunc(ns.Handler, 'GOSSIP_SHOW', function(self)
 		HBDP:AddWorldMapIconMap(self, PlayerMarker, UNDERBELLY, x, y)
 
 		for index = 1, GetNumGossipOptions() do
-			local text = select((index * 2) - 1, GetGossipOptions())
-			for i=1, table.getn(destinations) do
-				if(text:match(destinations[i].name)) then
-					local Marker = self:GetMarker()
-					Marker:SetID(index)
-					Marker:SetNormalAtlas('spell_arcane_portaldalarancrater')
-					Marker:SetHighlightAtlas('spell_arcane_portaldalaran')
-					Marker:SetSize(24)
-					Marker:SetName(destinations[i].name)
+			local s = select((index * 2) - 1, GetGossipOptions())
+			local t = destinations[s]
 
-					HBDP:AddWorldMapIconMap(self, Marker, UNDERBELLY, destinations[i].x, destinations[i].y)
-					break
-				end --if
-			end --for
+			local Marker = self:GetMarker()
+			Marker:SetID(index)
+			Marker:SetNormalAtlas('spell_arcane_portaldalarancrater')
+			Marker:SetHighlightAtlas('spell_arcane_portaldalaran')
+			Marker:SetSize(24)
+			Marker:SetName(s)
+
+			HBDP:AddWorldMapIconMap(self, Marker, UNDERBELLY, t.x, t.y)
 		end
 	end
 end)
