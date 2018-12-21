a9ac32c8347a206a351a380e1518c9e1c76bde4c
Adrian L Lange <mail@p3lim.net>
2018-12-21 03:19:12 +0100

    Add Arathi warfront data too

--- modules/Warfronts.lua
+++ modules/Warfronts.lua
@@ -3,6 +3,23 @@ local L = addon.L
 
 local HBD = LibStub('HereBeDragons-2.0')
 local npcData = {
+	[127068] = { -- Flightgineer Krazzle (Arathi, Horde)
+		mapID = 943,
+		source = {x = 0.5112, y = 0.4696},
+		destinations = {
+			[L['Ar\'gorok']]          = {x = 0.6020, y = 0.2237},
+			[L['Hatchet Ridge']]      = {x = 0.4539, y = 0.2213},
+			[L['Drywhisker Mine']]    = {x = 0.6997, y = 0.3329},
+			[L['Newstead']]           = {x = 0.4056, y = 0.4074},
+			[L['Circle of Elements']] = {x = 0.6831, y = 0.4755},
+		}
+	},
+	-- [132005] = { -- Grayson Bell (Arathi, Alliance)
+	-- 	mapID = 943,
+	-- 	source = {x = 0.0, y = 0.0},
+	-- 	destinations = {
+	-- 	}
+	-- },
 	[148565] = { -- Avery Nightwing (Darkshore, Horde)
 		mapID = 1203,
 		source = {x = 0.4861, y = 0.3447},
