8c85339239dbbf72a9d0b15298a40da3c68aab10
Adrian L Lange <mail@p3lim.net>
2019-01-14 04:11:25 +0100

    Yet another attempt at fixing russian locale issues
    Fixes #8

--- modules/Footholds.lua
+++ modules/Footholds.lua
@@ -7,9 +7,9 @@ local npcData = {
 		normalAtlas = 'bfa-landingbutton-horde-up',
 		highlightAtlas = 'bfa-landingbutton-horde-diamondhighlight',
 		destinations = {
-			[addon:GetMapName(896):lower()] = {x = 0.2061, y = 0.4569, zone = 896}, -- Drustvar
-			[addon:GetMapName(942):lower()] = {x = 0.5198, y = 0.2449, zone = 942}, -- Stormsong Valley
-			[addon:GetMapName(895):lower()] = {x = 0.8820, y = 0.5116, zone = 895}, -- Tiragarde Sound
+			[addon:GetMapName(896)] = {x = 0.2061, y = 0.4569, zone = 896}, -- Drustvar
+			[addon:GetMapName(942)] = {x = 0.5198, y = 0.2449, zone = 942}, -- Stormsong Valley
+			[addon:GetMapName(895)] = {x = 0.8820, y = 0.5116, zone = 895}, -- Tiragarde Sound
 		}
 	},
 	[135681] = { -- Grand Admiral Jes-Tereth
@@ -17,12 +17,17 @@ local npcData = {
 		normalAtlas = 'bfa-landingbutton-alliance-up',
 		highlightAtlas = 'bfa-landingbutton-alliance-shieldhighlight',
 		destinations = {
-			[addon:GetMapName(862):lower()] = {x = 0.4068, y = 0.7085, zone = 862}, -- Zuldazar
-			[addon:GetMapName(863):lower()] = {x = 0.6196, y = 0.4020, zone = 863}, -- Nazmir
-			[addon:GetMapName(864):lower()] = {x = 0.3560, y = 0.3317, zone = 864}, -- Vol'dun
+			[addon:GetMapName(862)] = {x = 0.4068, y = 0.7085, zone = 862}, -- Zuldazar
+			[addon:GetMapName(863)] = {x = 0.6196, y = 0.4020, zone = 863}, -- Nazmir
+			[addon:GetMapName(864)] = {x = 0.3560, y = 0.3317, zone = 864}, -- Vol'dun
 		}
 	}
 }
+
+if(GetLocale() == 'ruRU') then
+	-- why the hell are there two translations for "Stormsong Valley" in russian locale?
+	npcData[135690].destinations['долину Штормов'] = npcData[135690].destinations[addon:GetMapName(942)]
+end
 
 addon:Add(function(self)
 	local npcID = self:GetNPCID()
@@ -32,7 +37,7 @@ addon:Add(function(self)
 
 		for index, line in next, self:GetLines() do
 			for name, loc in next, data.destinations do
-				if(line:lower():match(name)) then
+				if(line:match(name)) then
 					local Marker = self:NewMarker()
 					Marker:SetID(index)
 					Marker:SetNormalAtlas(data.normalAtlas)
