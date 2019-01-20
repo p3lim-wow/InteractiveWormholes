d9d1e25cff1818bce24df4445590cb60e1df1e0b
Adrian L Lange <mail@p3lim.net>
2019-01-20 23:55:08 +0100

    Use existing map names

--- modules/Skyhold.lua
+++ modules/Skyhold.lua
@@ -3,13 +3,13 @@ local L = addon.L
 
 local BROKENISLES = 619
 local destinations = {
-	[L['Dalaran']] 			= {zone = 627, x = 0.7246, y = 0.4594},
-	[L['Stormheim']] 		= {zone = 634, x = 0.6034, y = 0.5106},
-	[L['Azsuna']] 			= {zone = 630, x = 0.4749, y = 0.2755},
-	[L['Val\'sharah']] 		= {zone = 641, x = 0.5462, y = 0.7313},
-	[L['Highmountain']] 	= {zone = 750, x = 0.3853, y = 0.4554},
-	[L['Suramar']] 			= {zone = 680, x = 0.3381, y = 0.4940},
-	[L['Broken Shore']] 	= {zone = 646, x = 0.4427, y = 0.6299},
+	[addon:GetMapName(627)]	= {zone = 627, x = 0.7246, y = 0.4594}, -- Dalaran
+	[addon:GetMapName(634)]	= {zone = 634, x = 0.6034, y = 0.5106}, -- Stormheim
+	[addon:GetMapName(630)]	= {zone = 630, x = 0.4749, y = 0.2755}, -- Azsuna
+	[addon:GetMapName(641)]	= {zone = 641, x = 0.5462, y = 0.7313}, -- Val'sharah
+	[addon:GetMapName(750)]	= {zone = 750, x = 0.3853, y = 0.4554}, -- Highmountain
+	[addon:GetMapName(680)]	= {zone = 680, x = 0.3381, y = 0.4940}, -- Suramar
+	[addon:GetMapName(646)]	= {zone = 646, x = 0.4427, y = 0.6299}, -- Broken Shore
 }
 
 addon:Add(function(self)
