486baa50dee771d78d6a326337fe9f93ef54a0f8
Adrian L Lange <mail@p3lim.net>
2020-08-08 23:25:48 +0200

    Drop the symlinks, filter lua errors instead

--- utils/link.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")/.."
-
-[[ ! -d libs ]] && mkdir -p libs
-[[ ! -L libs/LibStub ]] && ln -s ../LibStub libs/LibStub
-[[ ! -L libs/CallbackHandler-1.0 ]] && ln -s ../CallbackHandler-1.0 libs/CallbackHandler-1.0
-[[ ! -L libs/HereBeDragons ]] && ln -s ../HereBeDragons libs/HereBeDragons
-[[ ! -L libs/LibBabble-SubZone-3.0 ]] && ln -s ../LibBabble-SubZone-3.0 libs/LibBabble-SubZone-3.0
