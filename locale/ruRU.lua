f37d30dbf694d183fa15684cfbe80b133d593451
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2021-04-30 09:23:16 +0300

    Update ruRU.lua
    Russian UPD

--- locale/ruRU.lua
+++ locale/ruRU.lua
@@ -2,27 +2,27 @@ if(GetLocale() ~= 'ruRU') then return end
 local L = select(2, ...).L('ruRU')
 
 --- general strings
--- L['Click to travel'] = '' -- MISSING TRANSLATION
--- L['You will end up in one of multiple locations within this zone!'] = '' -- MISSING TRANSLATION
--- L['The rocket might not land exactly where you\'d expect, nor take the path exactly as shown.'] = '' -- MISSING TRANSLATION
+L['Click to travel'] = 'Нажмите, чтобы путешествовать'
+L['You will end up in one of multiple locations within this zone!'] = 'Вы окажетесь в одном из нескольких мест в этой локации!'
+L['The rocket might not land exactly where you\'d expect, nor take the path exactly as shown.'] = 'Ракета может приземлиться не совсем там, где вы ожидаете, и не пройти путь точно так, как показано на рисунке.'
 L['Not Discovered'] = TAXI_PATH_UNREACHABLE
 L['You are here'] = TAXINODEYOUAREHERE
 
 --- Subzones missing in LibBabble-SubZone
 L['Throne of Flame'] = 'Трон Пламени'
 L['Fel Pits'] = 'Ямы Скверны'
--- L['Blackrock Foundry Overlook'] = '' -- MISSING TRANSLATION
--- L['Stormgarde Keep'] = '' -- MISSING TRANSLATION
--- L['Gloomtide Strand'] = '' -- MISSING TRANSLATION
--- L['Cinderfall Grove'] = '' -- MISSING TRANSLATION
--- L['Lor\'danel Landing'] = '' -- MISSING TRANSLATION
--- L['Northern Rocketway Terminus'] = '' -- MISSING TRANSLATION
--- L['Gallywix Rocketway Exchange'] = '' -- MISSING TRANSLATION
+L['Blackrock Foundry Overlook'] = 'Яма'
+L['Stormgarde Keep'] = 'Стромгард'
+L['Gloomtide Strand'] = 'Темные Берега'
+L['Cinderfall Grove'] = 'Пепельная роща'
+L['Lor\'danel Landing'] = 'Лор\'данел'
+L['Northern Rocketway Terminus'] = 'Северный конечный пункт ракетной дороги'
+L['Gallywix Rocketway Exchange'] = 'Ракетная дорога Галливикса'
 
 --- Dalaran Sewer Portals
--- L['Sewer Guard Station'] = 'пост стража канализации' -- UNCONFIRMED TRANSLATION
--- L['Black Market'] = 'черный рынок' -- UNCONFIRMED TRANSLATION
--- L['Inn Entrance'] = 'вход в таверну' -- UNCONFIRMED TRANSLATION
--- L['Alchemists\' Lair'] = 'логово алхимика' -- UNCONFIRMED TRANSLATION
--- L['Abandoned Shack'] = 'заброшенная лачуга' -- UNCONFIRMED TRANSLATION
--- L['Rear Entrance'] = 'черный ход' -- UNCONFIRMED TRANSLATION
+L['Sewer Guard Station'] = 'Пост стража канализации'
+L['Black Market'] = 'Черный Рынок'
+L['Inn Entrance'] = 'Вход в таверну'
+L['Alchemists\' Lair'] = 'Логово алхимика'
+L['Abandoned Shack'] = 'Заброшенная лачуга'
+L['Rear Entrance'] = 'Черный ход'
