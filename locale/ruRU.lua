059a2918b459fb56292512b93b3dff249fe83b3b
Adrian L Lange <git@p3lim.net>
2024-12-19 02:44:43 +0100

    Don't keep unfinished strings in comments

--- locale/ruRU.lua
+++ locale/ruRU.lua
@@ -1,11 +1,1 @@
-local L = select(2, ...).L('ruRU')
-
---- settings
--- L['Change maps'] = '' -- MISSING TRANSLATION
--- L['Change to the most appropriate map.\n\n|cffff0000Warning:|r This will cause taint!'] = '' -- MISSING TRANSLATION
--- L['Map pin scale'] = '' -- MISSING TRANSLATION
--- L['The scale of pins on the map'] = '' -- MISSING TRANSLATION
--- L['Pin size zoom factor'] = '' -- MISSING TRANSLATION
--- L['How much extra scale to apply when map is zoomed'] = '' -- MISSING TRANSLATION
--- L['Taxi world map'] = '' -- MISSING TRANSLATION
--- L['Use the normal world map for taxi'] = '' -- MISSING TRANSLATION
+local L = select(2, ...).L('ruRU') -- luacheck: ignore
