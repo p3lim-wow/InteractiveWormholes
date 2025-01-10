41a7dca044ce5b4d0b7c7a3fd561205f0190f027
Adrian L Lange <git@p3lim.net>
2025-01-10 22:31:35 +0100

    Split API out to its own file

---
+++ api.lua
@@ -0,0 +1,25 @@
+
+-- expose a namespace
+InteractiveWormholes = {}
+
+-- expose an API for other addons to use to check if we're active
+function InteractiveWormholes:IsActive()
+	if IsShiftKeyDown() or InCombatLockdown() then
+		-- we'll never be active if the user holds shift or if the player is in combat
+		return false
+	end
+
+	if addon.stagedGossipOptionID ~= nil then
+		-- there's a staged option, we're still active
+		return true
+	end
+
+	for _, gossipInfo in next, C_GossipInfo.GetOptions() do
+		if addon.data[gossipInfo.gossipOptionID or 0] then
+			-- there's an option we manage, we're active
+			return true
+		end
+	end
+
+	return false
+end
