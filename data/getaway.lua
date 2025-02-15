ccddeca04666ea770c47086565817061b7f1457d
Adrian L Lange <git@p3lim.net>
2025-02-15 19:16:16 +0100

    Fix getaway tooltips

--- data/getaway.lua
+++ data/getaway.lua
@@ -5,36 +5,43 @@ addon.data[114307] = { -- Winterspring
 	mapID = 83,
 	x = 0.2278,
 	y = 0.4676,
+	tooltipMap = 83,
 }
 addon.data[114308] = { -- Jade Forest
 	mapID = 371,
 	x = 0.4391,
 	y = 0.4226,
+	tooltipMap = 371,
 }
 addon.data[114309] = { -- Val'sharah
 	mapID = 641,
 	x = 0.5152,
 	y = 0.5844,
+	tooltipMap = 641,
 }
 addon.data[119770] = { -- Northern Stranglethorn
 	mapID = 50,
 	x = 0.4500,
 	y = 0.4179,
+	tooltipMap = 50,
 }
 addon.data[119852] = { -- Feralas
 	mapID = 69,
 	x = 0.7000,
 	y = 0.7449,
+	tooltipMap = 69,
 }
 addon.data[119853] = { -- Nagrand
 	mapID = 107,
 	x = 0.4960,
 	y = 0.4640,
+	tooltipMap = 107,
 }
 addon.data[119854] = { -- Grizzly Hills
 	mapID = 116,
 	x = 0.7689,
 	y = 0.4905,
+	tooltipMap = 116,
 }
 addon.data[114331] = addon.data[119852] -- Feralas (quest version)
 addon.data[114330] = addon.data[119853] -- Nagrand (quest version)
