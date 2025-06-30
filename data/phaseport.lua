b39abbc5650b668264e149ac101ca4c45b91a9fd
Adrian L Lange <git@p3lim.net>
2025-06-30 20:59:48 +0200

    Add support for phase porting

---
+++ data/phaseport.lua
@@ -0,0 +1,85 @@
+local _, addon = ...
+
+addon.data[134553] = { -- North Sufaad
+	mapID = 2371, -- K'aresh
+	x = 0.7717,
+	y = 0.4893,
+	tooltipArea = 15795,
+	atlas = 'flightmaster_ancientwaygate-taxinode_neutral',
+	highlightAdd = true,
+	forceMapID = 2371,
+}
+addon.data[134560] = { -- Overlook Zo'Shuul
+	mapID = 2371, -- K'aresh
+	x = 0.5041,
+	y = 0.3640,
+	tooltipArea = 15807,
+	atlas = 'flightmaster_ancientwaygate-taxinode_neutral',
+	highlightAdd = true,
+	forceMapID = 2371,
+}
+addon.data[134559] = { -- Serrated Peaks
+	mapID = 2371, -- K'aresh
+	x = 0.5121,
+	y = 0.4856,
+	tooltipArea = 15806,
+	atlas = 'flightmaster_ancientwaygate-taxinode_neutral',
+	highlightAdd = true,
+	forceMapID = 2371,
+}
+addon.data[134558] = { -- Shadow Point
+	mapID = 2371, -- K'aresh
+	x = 0.4521,
+	y = 0.2391,
+	tooltipArea = 15805,
+	atlas = 'flightmaster_ancientwaygate-taxinode_neutral',
+	highlightAdd = true,
+	forceMapID = 2371,
+}
+addon.data[134557] = { -- Shan'dorah
+	mapID = 2371, -- K'aresh
+	x = 0.6021,
+	y = 0.2904,
+	tooltipArea = 15802,
+	atlas = 'flightmaster_ancientwaygate-taxinode_neutral',
+	highlightAdd = true,
+	forceMapID = 2371,
+}
+addon.data[134556] = { -- Tazavesh
+	mapID = 2472, -- Tazavesh
+	x = 0.4678,
+	y = 0.5685,
+	tooltipArea = 15781,
+	atlas = 'flightmaster_ancientwaygate-taxinode_neutral',
+	highlightAdd = true,
+	forceMapID = 2371,
+}
+addon.data[134555] = { -- Oasis
+	mapID = 2371, -- K'aresh
+	x = 0.7590,
+	y = 0.3297,
+	tooltipArea = 15792,
+	atlas = 'flightmaster_ancientwaygate-taxinode_neutral',
+	highlightAdd = true,
+	forceMapID = 2371,
+}
+addon.data[134554] = { -- Vanquisher's Wake
+	mapID = 2371, -- K'aresh
+	x = 0.7126,
+	y = 0.0820,
+	tooltipArea = 16081,
+	atlas = 'flightmaster_ancientwaygate-taxinode_neutral',
+	highlightAdd = true,
+	forceMapID = 2371,
+}
+addon.data[135183] = { -- "<Return to normal space.>"
+	mapID = 2371, -- K'aresh
+	x = 0.2,
+	y = 0.7,
+	atlas = 'Crosshair_VehichleCursor_96',
+	atlasSize = 96,
+	highlightAdd = true,
+	noArrow = true,
+	ignoreScale = true,
+	forceMapID = 2371,
+}
