2a1fdb334f43a007840cd689820042b09849c649
Adrian L Lange <git@p3lim.net>
2026-03-03 22:37:19 +0100

    Just set the thickness by default, we don't change it ever

--- pools/line.lua
+++ pools/line.lua
@@ -7,19 +7,16 @@ lineParent:SetFrameLevel(2200) -- need to set it high so lines render above the 
 local linePool = CreateUnsecuredObjectPool(function()
 	local line = lineParent:CreateLine()
 	line:SetAtlas('_UI-Taxi-Line-horizontal')
-
-	-- scale the line with the canvas
-	line:SetThickness(1 / WorldMapFrame:GetCanvasScale() * 35)
 	return line
 end, function(_, line)
 	line:Hide()
 end)
 
-function addon:AttachLine(source, destination, thickness)
+function addon:AttachLine(source, destination)
 	local line = linePool:Acquire()
 	line:SetStartPoint('CENTER', source)
 	line:SetEndPoint('CENTER', destination)
-	line:SetThickness(thickness)
+	line:SetThickness(1 / WorldMapFrame:GetCanvasScale() * 35)
 	line:Show()
 end
 
